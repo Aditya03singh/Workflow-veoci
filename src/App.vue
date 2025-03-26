e3c44c5c36a797d3450a95b2eee612329f3fe22c
Aditya03singh <adisingh0309@gmail.com>
2025-03-26 14:09:37 +0530

    final commit

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,17 @@
 <template>
   <router-view />
 </template>
-
 <script>
-
   export default {
     name:"App",
     data(){
       return{
-
       }
     },
     components:{
     }
   }
 </script>
-
-<style lang="scss" scoped>
+<style>
 
 </style>
