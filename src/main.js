20796175ac7b2fdb065ef43d13b5930e397891e0
Aditya03singh <adisingh0309@gmail.com>
2025-03-27 12:18:10 +0530

    Workflow show and edit button updated

--- src/main.js
+++ src/main.js
@@ -6,11 +6,14 @@ import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
 import Workflow from './components/Workflow.vue'
 import Workflow_launch from './components/Workflow_launch.vue'
+import Workflow_edit from './components/Workflow_edit.vue'
 
 const routes = [
     {path:"/", name:"workflow", component:Workflow},
     {path:"/create_workflow", name:"createWorkflow", component:Workflow},
-    {path:"/create_workflow/launch_workflow", name:"launchWorkflow" , component:Workflow_launch}
+    {path:"/show_workflow", name:"showWorkflow", component:Workflow},
+    {path:"/create_workflow/launch_workflow", name:"launchWorkflow" , component:Workflow_launch},
+    {path:"/edit_workflow", name:"editWorkflow", component:Workflow_edit}
 ]
 
 
